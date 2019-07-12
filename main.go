30956cbfbc278b23c876827ab90230230fb15d7c
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-07-12 13:03:18 -0700

    Added baseline components

--- main.go
+++ main.go
@@ -1,23 +1,19 @@
 package main
 
 import (
-        "log"
-        ui "github.com/gizak/termui"
+        ui
 )
 
 const (
-	VERSION = "0.0.0"
-	//USAGE   = 
+    VERSION = "0.0.0"
+    // USAGE = ``
 )
 
 func init() {
+
 }
 
 func main() {
-    if err := ui.Init(); err != nil {
-        log.Fatalln(err)
-    }
-
-    defer ui.Close()
+    
 }
 
